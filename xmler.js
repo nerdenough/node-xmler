6e007ca3af8c43d0020489638959f0b35b4db1cb
Brendan Goodenough <brendan@goodenough.nz>
2016-03-08 20:34:58 +1300

    Add xmler.js allowing creation of elements
    The element currently has the ability to add attributes (singular or
    from an array) as well as adding child elements.

---
+++ xmler.js
@@ -0,0 +1,62 @@
+// XMLer is a simple package that allows the creation of XML elements within
+// JavaScript. It allows the addition of attributes as well as nested child
+// elements.
+//
+// Author: Brendan Goodenough <brendan@goodenough.nz>
+// GitHub: https://github.com/nerdenough/node-xmler
+// License: MIT
+
+// Creates the element with a specified name and initialises the arrays for
+// attributes and child elements.
+var Element = function (name) {
+  this.name = name;
+  this.attributes = [];
+  this.elements = [];
+};
+
+// Adds an attribute onto the attributes array. These attributes are added as a
+// key/value pair.
+Element.prototype.addAttribute = function (key, value) {
+  this.attributes.push({
+    key: key,
+    value: value
+  });
+};
+
+// Adds a single child element to the elements array.
+Element.prototype.addElement = function (element) {
+  this.elements.push(element);
+};
+
+// Adds an array of child elements to the elements array.
+Element.prototype.addElements = function (elements) {
+  this.elements.concat(elements);
+};
+
+// Builds an XML string for the element by iterating over all the attributes
+// and child elements.
+Element.prototype.getXML = function () {
+  var xml = '<' + this.name;
+
+  this.attributes.forEach(function (attribute) {
+    xml += ' ' + attribute.key + '="' + attribute.value + '"';
+  });
+
+  if (this.elements.length) {
+    xml += '>';
+
+    this.elements.forEach(function (element) {
+      xml += element.getXML();
+    });
+
+    xml += '</' + this.name + '>';
+  } else {
+    xml += '/>';
+  }
+
+  return xml;
+};
+
+module.exports = {
+  Element: Element
+};
